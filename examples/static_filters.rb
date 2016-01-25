4f33ec472ae6a3a69706b431664ee7ccf5d8c34f
Adrian Cheater <adrian.cheater@gmail.com>
2016-01-25 13:21:11 -0600

    Added a part 2, nearly time to start breaking these up into sub files

--- examples/static_filters.rb
+++ examples/static_filters.rb
@@ -23,7 +23,7 @@ use_bpm 140
 # different synths in 'compare_synths'
 
 define :compare_synths do
-  3.times do
+  4.times do
     %i(sine saw square tri).each do |synth|
       use_synth synth
       play :c2
@@ -48,9 +48,46 @@ compare_synths
 #
 # Sine waves
 # The documentation for Sonic Pi talks about sine synths as being 'pure' notes.
-# But what does that mean? It means that the wave form doesn't have any harmonics built into it.
+# But what does that mean? It means that the wave form doesn't have any harmonics included.
+# What is a harmonic? A harmonic is a multiple of the frequency of the note you're trying to play.
+# That note is the value you pass to the play commands, and is also known as the 'fundamental frequency'
+# Harmonics are always higher frequency (higher pitched) versions of wave.
 #
+# So when you play a note with a sine synth, you are only hearing that fundamental frequency, nothing else.
+# All other waves can be made by mixing together several sine waves at different frequencies and strengths,
+# and each new sine wave added into the mix will change the note's timbre.
+# When you're trying to create a new sound by playing several sine waves at the same time, that's called
+# "Additive Synthisys", and is one way to make things sound new. Here's an example.
+
+define :add_sine do
+  use_synth :sine
+
+  10.times do |i|
+    use_random_seed 37
+    base_freq = midi_to_hz(:c2)
+    notes = range(2,11).shuffle
+
+    play hz_to_midi(base_freq), sustain: 1.0, amp: 1.0
+    i.times do |j|
+      play hz_to_midi(base_freq*notes[j]), amp: 1.0/(j+2), sustain: 1.0, release: 0.1
+    end
+    sleep 1.5
+  end
+end
+add_sine
+
+# When you listen to it, it's adding random harmonics at diminishing strengths
+# Even though the fundamental :c2 is the loudest single waveform, you can hear
+# how the timbre and sometimes even the perceived pitch changes as more waves
+# are introduced. Next time we'll look at some other waves that aren't produced
+# with additive synthisys, but who's shape contains many harmonics within them.
+
+
+# Synths, Timbre and Filters Part 2
+# By Adrian Cheater
 #
+# Saw waves
+# A saw tooth wave, named for its shape, isn't...
 #
 #
 #
@@ -60,11 +97,7 @@ compare_synths
 # Give the program a listen a few times, and then continue reading
 #
 #
-# What is a harmonic? A harmonic is a multiple of the note you're trying to play.
-# That note is the value you pass to the play commands, and is also known as the 'fundamental frequency'
-# Harmonics are always higher frequency (higher pitched) versions of wave.
-# So when you play a note with a sine synth, you are only hearing that fundamental frequency, nothing else.
-#
+
 # When you combine the harmonics of a sine wave, different shapes start to take form.
 # While sonic pi isn't making those waveforms by adding sine waves together, the fact that
 # these waves can be made by combining harmonics means that to our ears, all those different
